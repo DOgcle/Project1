5e62d08c5fa2bbd487fae955cacfc0c65f69405a
Korotodan <taidangtan8@gmail.com>
2023-03-04 16:28:35 +0700

    xong

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,162 +1,232 @@
 ﻿#include <iostream>
 #include <vector>
 #include <math.h>
+#include <list>   
 
 using namespace std;
 using std::vector;
 
-class School {
-public:
-	string name;
-public:
-	School(){}
-	School(string n) {
-		this->name = n;
-	}
-	
-};
-class Class {
-public:
-	int ss;
-	string name;
-
-
-	
-public:
-	Class() {}
-	Class(string n, int s) {
-		this->name = n;
-		this->ss = s;
-		
-	}
-	void display() {
-		cout << "Name: " << name << endl;
-		cout << "Number: " << ss << endl;
-	}
-	static void  getMaxStudent(vector<Class> c) {
-		int count = 0;
-		double max = c[0].ss;
-		vector <Class> v;
-		// chạy tìm điểm cao nhất
-		for (int i = 0; i < c.size(); i++) {
-			if (count < c[i].ss) {
-				max = c[i].ss;
-			}
-		}
-		// thêm vào danh sách 
-		for (int i = 0; i < c.size(); i++) {
-			if (c[i].ss == max) {
-				v.push_back(c[i]);
-			}
-		}
-		// in ra sinh viên cao nhất
-		for (int i = 0; i < v.size(); i++) {
-			v[i].display();
-		}
-
-
-	}
-
-	static void  getMinStudent(vector<Class> c) {
-		
-		double max = c[0].ss;
-		vector <Class> v;
-		// chạy tìm điểm cao nhất
-		for (int i = 0; i < c.size(); i++) {
-			if (max > c[i].ss) {
-				max = c[i].ss;
-			}
-		}
-		// thêm vào danh sách 
-		for (int i = 0; i < c.size(); i++) {
-			if (c[i].ss == max) {
-				v.push_back(c[i]);
-			}
-		}
-		// in ra sinh viên cao nhất
-		for (int i = 0; i < v.size(); i++) {
-			v[i].display();
-		}
-
-
-	}
-	
-	
-
-};
 class Student {
-public:
+private:
 	string name;
 	float avgs;
-	
+
 public:
 	Student() {}
-	
-	Student(string n, float a) {
-		this->name = n;
-		this->avgs = a;
+
+	Student(string name, float avgs) {
+		this->name = name;
+		this->avgs = avgs;
+	}
+	string getName() {
+		return name;
+	}
+	float getAvgs() {
+		return avgs;
 	}
 	void display() {
 		cout << "Name: " << name << endl;
 		cout << "Average Score: " << avgs << endl;
 	}
 
-	static void  theHighestAverageScore(vector<Student> listStudent) {
-		int count = 0;
-		double max = listStudent[0].avgs;
+	
+
+
+};
+
+
+class Class {
+private:
+	int ss;
+	int numberGood;
+	string name;
+	vector <Student> listStudent;
+	bool good;
+
+public:
+	Class() {}
+	Class(string n, int s) {
+		this->name = n;
+		this->ss = s;
+		this->good = false;
+		this->numberGood = 0;
+		this->listStudent = vector<Student>();
+	}
+	string getName() {
+		return name;
+	}
+	int getSs() {
+		return ss;
+	}
+	vector<Student> getListStudent() {
+		return listStudent;
+	}
+	bool getGood() {
+		return good;
+	}
+	int getNumberGood() {
+		return numberGood;
+	}
+
+	void display() {
+		cout << "Name: " << name << endl;
+		cout << "Number: " << ss << endl;
+	}
+
+	void add(Student s) {
+		listStudent.push_back(s);
+	}
+
+	
+
+	 void  getTheHighestAverageScore() {
+		double max = listStudent[0].getAvgs();
 		vector <Student> v;
 		// chạy tìm điểm cao nhất
 		for (int i = 0; i < listStudent.size(); i++) {
-			if (count < listStudent[i].avgs) {
-				max = listStudent[i].avgs;
+			if (max < listStudent[i].getAvgs()) {
+				max = listStudent[i].getAvgs();
 			}
 		}
 		// thêm vào danh sách 
 		for (int i = 0; i < listStudent.size(); i++) {
-			if (listStudent[i].avgs == max) {
+			if (listStudent[i].getAvgs() == max) {
 				v.push_back(listStudent[i]);
 			}
 		}
 		// in ra sinh viên cao nhất
 		for (int i = 0; i < v.size(); i++) {
+			cout << "The Highest Average Score " << endl;
 			v[i].display();
 		}
 
 
 	}
-	static bool good() {
-		return true;
-	}
-	static void goodSummary(vector<Student>student) {
-		int count = 0;
-		for (int i = 0; i < student.size(); i++) {
-			if (student[i].avgs >= 8) {
-				count++;
-
+	
+	 void getGoodSummary() {
+		
+		for (int i = 0; i < listStudent.size(); i++) {
+			if (listStudent[i].getAvgs() >= 8) {
+				numberGood++;
 			}
 		}
-		if (count > 5)
-			good();
-		cout << "Summary" << count << endl;
+	}
+	 
+	 void getSummary() {
+		 getGoodSummary();
+		 if (numberGood > 5)
+			 good = true;
+	 }
+
+};
+
+
+class School {
+private:
+	string name;
+	vector <Class> listClass;
+public:
+	School() {}
+	School(string n) {
+		this->name = n;
+		this->listClass = vector<Class>();
+	}
+
+	void add(Class c) {
+		listClass.push_back(c);
+	}
+
+	void  getMaxStudent() {
+		int count = 0;
+		double max = listClass[0].getSs();
+		vector <Class> v;
+		// chạy tìm lớp đông nhất
+		for (int i = 0; i < listClass.size(); i++) {
+			if (count < listClass[i].getSs()) {
+				max = listClass[i].getSs();
+			}
+		}
+		// thêm vào danh sách 
+		for (int i = 0; i < listClass.size(); i++) {
+			if (listClass[i].getSs() == max) {
+				v.push_back(listClass[i]);
+			}
+		}
+		// in ra lớp đông nhất
+		for (int i = 0; i < v.size(); i++) {
+			cout << "The Most Crowded Class" << endl;
+			v[i].display();
+		}
+
 
 	}
 
-	static void getClassGood(vector<Class> c) {
-		for (int i = 0; i < c.size(); i++) {
-			if (good() == true) {
-				c[i].display();
+	void  getMinStudent() {
+
+		double max = listClass[0].getSs();
+		vector <Class> v;
+		// chạy tìm điểm cao nhất
+		for (int i = 0; i < listClass.size(); i++) {
+			if (max > listClass[i].getSs()) {
+				max = listClass[i].getSs();
+			}
+		}
+		// thêm vào danh sách 
+		for (int i = 0; i < listClass.size(); i++) {
+			if (listClass[i].getSs() == max) {
+				v.push_back(listClass[i]);
+			}
+		}
+		// in ra sinh viên cao nhất
+		for (int i = 0; i < v.size(); i++) {
+			cout << " The class at least" << endl;
+			v[i].display();
+		}
+
+	}
+
+	void getMaxAvgs() {
+		int max = listClass[0].getNumberGood();
+		vector <Class> v;
+
+		for (int i = 0; i < listClass.size(); i++) {
+			listClass[i].getGoodSummary();
+			if (max < listClass[i].getNumberGood()) {
+				max = listClass[i].getNumberGood();
+			}
+		}
+
+		for (int i = 0; i < listClass.size(); i++) {
+			if (listClass[i].getSs() == max) {
+				v.push_back(listClass[i]);
+			}
+		}
+		// in ra sinh viên cao nhất
+		for (int i = 0; i < v.size(); i++) {
+			cout << " Best Class:" << endl;
+			v[i].display();
+		}
+
+	}
+
+	void getClassGood() {
+		cout << "Class Good" << endl;
+		for (int i = 0; i < listClass.size(); i++) {
+			listClass[i].getSummary();
+			if (listClass[i].getGood()== true) {
+				listClass[i].display();
 			}
 		}
 	}
 
 
 };
+
+
 int main() {
-	vector <Student> A;
-	vector<Student> B;
-	vector <Class> c;
-	Class c1("A", 10);
-	Class c2("B", 11);
+	School sch ("HCMUS");
+	Class c1 ("A", 10);
+	Class c2 ("B", 11);
 	Student s = Student("Bob", 4.5);
 	Student s1 = Student("Jack", 6.5);
 	Student s2 = Student("Kenny", 8);
@@ -168,10 +238,10 @@ int main() {
 	Student s8 = Student("A1", 10);
 	Student s9 = Student("B1", 2);
 	Student s10 = Student("C1", 10);
-	Student s11 = Student("D1", 3);
-	Student s12 = Student("E1", 4);
-	Student s13 = Student("Q1", 7);
-	Student s14 = Student("W1", 6);
+	Student s11 = Student("D1", 9);
+	Student s12 = Student("E1", 8);
+	Student s13 = Student("Q1", 8.5);
+	Student s14 = Student("W1", 10);
 	Student s15 = Student("R1", 5);
 	Student s16 = Student("T1", 8);
 	Student s17 = Student("Y1", 9);
@@ -180,29 +250,31 @@ int main() {
 	Student s20 = Student("O1", 3);
 	Student s21 = Student("G1", 5);
 
-	c.push_back(c1);
-	c.push_back(c2);
+	c1.add(s);
+	c1.add(s1);
+	c1.add(s2);
+	c1.add(s3);
+	c1.add(s4);
+	c1.add(s5);
+	c1.add(s6);
 
-	
-
-	A.push_back(s);
-	A.push_back(s1);
-	A.push_back(s2);
-
-	B.push_back(s3);
-	B.push_back(s5);
-	B.push_back(s19);
-	B.push_back(s17);
-	B.push_back(s16);
-	B.push_back(s8);
+	c2.add(s10);
+	c2.add(s11);
+	c2.add(s12);
+	c2.add(s13);
+	c2.add(s14);
+	c2.add(s15);
+	c2.add(s16);
 
 
-	Class::getMaxStudent(c);
-	Class::getMinStudent(c);
+	c1.getTheHighestAverageScore();
 
-	Student::theHighestAverageScore(A);
-	/*Class::goodSummary(c);*/
-
+	sch.add(c1);
+	sch.add(c2);
+	sch.getMaxStudent();
+	sch.getClassGood();
+	sch.getMaxAvgs();
+	sch.getMinStudent();
 
 	return 0;
 }
